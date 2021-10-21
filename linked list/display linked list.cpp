d7738e0d2aa3137c3b7ab67703b3c763b1c24378
sudarsan2060 <85069932+sudarsan2060@users.noreply.github.com>
2021-10-21 10:06:32 +0530

    Update display linked list.cpp

--- linked list/display linked list.cpp
+++ linked list/display linked list.cpp
@@ -1,1 +1,38 @@
 /* create and display linked list*/
+#include <bits/stdc++.h>
+using namespace std;
+class node{
+  public:
+  int data;
+  node *next;
+};
+void display(node *p){
+  while(p!=0){
+    cout<<p->data<<endl;
+    p=p->next;
+  }
+}
+int main(){
+  node *head=NULL;
+  node *second=NULL;
+  node *third=NULL;
+  head=new node();
+  second=new node();
+  third=new node();
+  head->data=10;
+  head->next=second;
+  second->data=20;
+  second->next=thrd;
+  third->data=30;
+  third->next=NULL;
+  display(head);
+  return 0;
+  )
+    
+    
+    /* out put */
+    /*
+    10
+    20
+    30
+    */
