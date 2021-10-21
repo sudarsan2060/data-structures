8a90879f3f2849d6a8ed6386f35b949d74d48a04
sudarsan2060 <85069932+sudarsan2060@users.noreply.github.com>
2021-10-21 10:40:55 +0530

    Create insert after any position.cpp

---
+++ linked list/insert after any position.cpp
@@ -0,0 +1,53 @@
+ #include <cstddef>
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+    Node * next;
+};
+
+void print_list(Node * n) {
+    cout << "\nPrinting new list..." << endl;
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+} 
+
+void insertAfter( Node * prev_node, int new_data) {
+    if (prev_node == NULL) {
+        printf("the given previous node cannot be NULL");
+        return;
+    }
+    Node *new_node;
+     new_node=new Node();
+    new_node->data = new_data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+}
+
+int main() {
+    Node * head = NULL;
+    Node * second = NULL;
+    Node * third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    print_list(head);
+    insertAfter(second, 11);
+    print_list(head);
+}
