1788f0ba09cc2cb7a703f2f9255ac86345dbccbb
sudarsan2060 <85069932+sudarsan2060@users.noreply.github.com>
2021-10-21 15:01:45 +0530

    complete function

--- linked list/insert in sorted list.cpp
+++ linked list/insert in sorted list.cpp
@@ -1,29 +1,77 @@
 
 ****************inserting in sorted list****************
 
+ Inserting in a Sorted Linked List
 
- void sortedinsertion(node *p,int x){
-    node *t=new node();
-    node *q=NULL;
-    if(first==0){
-        first=t;
-    }
-    
-    else {
-    while(p&&p->data<x){
-        q=p;
-        p=p->next;
-    }
-    if(p==first){
-        t->next=first;
-        first=t;
-    }
-    else{
-    t->next=q->next;
-    q->next=t;
-        
-    }
-        
-    }
-    
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+int data;
+struct Node *next;
+}*first=NULL,*second=NULL,*third=NULL;
+void Display(struct Node *p)
+{
+while(p!=NULL)
+{
+printf("%d ",p->data);
+p=p->next;
+}
+}
+
+void create(int A[],int n)
+{
+int i;
+struct Node *t,*last;
+first=(struct Node *)malloc(sizeof(struct Node));
+first->data=A[0];
+first->next=NULL;
+last=first;
+for(i=1;i<n;i++)
+{
+t=(struct Node*)malloc(sizeof(struct Node));
+t->data=A[i];
+t->next=NULL;
+last->next=t;
+last=t;
+}
+}
+
+void SortedInsert(struct Node *p,int x)
+{
+struct Node *t,*q=NULL;
+
+t=(struct Node*)malloc(sizeof(struct Node));
+t->data=x;
+t->next=NULL;
+if(first==NULL)
+first=t;
+else
+{
+while(p && p->data<x)
+{
+q=p;
+p=p->next;
+}
+if(p==first)
+{
+t->next=first;
+first=t;
+}
+else
+{
+t->next=q->next;
+q->next=t;
+}
+}
+}
+int main()
+{
+int A[]={10,20,30,40,50};
+create(A,5);
+
+printf(“%d\n”,SortedInsert(first,15));
+Display(first);
+
+return 0;
 }
